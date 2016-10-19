53d88f842e79bda646c9dc6eb05d8623068cb82c
Frank <akura11.tt@gmail.com>
2016-10-19 18:30:39 -0500

    Adapatando solr con tomcat

--- ckan-postgres/init_geospatial.sh
+++ ckan-postgres/init_geospatial.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Creacion de tablas y funciones postgis
-psql --username "$POSTGRES_USER" -d $POSTGRES_DB -f /usr/share/postgresql/9.3/contrib/postgis-2.2/postgis.sql
-psql --username "$POSTGRES_USER" -d $POSTGRES_DB -f /usr/share/postgresql/9.3/contrib/postgis-2.2/spatial_ref_sys.sql
+psql --username "$POSTGRES_USER" -d $POSTGRES_DB -f /usr/share/postgresql/9.3/contrib/postgis-2.3/postgis.sql
+psql --username "$POSTGRES_USER" -d $POSTGRES_DB -f /usr/share/postgresql/9.3/contrib/postgis-2.3/spatial_ref_sys.sql
 
 # Ajustando permisos de las tablas geoespacioles
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d $POSTGRES_DB<<-EOSQL
